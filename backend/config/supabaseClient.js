5fe6677fd091a79ce4da304500b9a9951f0c637b
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2025-07-24 18:14:52 +0800

    Connect Database

---
+++ backend/config/supabaseClient.js
@@ -0,0 +1,8 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ptxqlcypckufredcmnmv.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0eHFsY3lwY2t1ZnJlZGNtbm12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNDY4MTgsImV4cCI6MjA2ODkyMjgxOH0.r58lzMSX62-wbZ754Q0X2Wzqy-MQBm_oShd36DDzvn0'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
